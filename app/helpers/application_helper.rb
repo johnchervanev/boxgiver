bilal1976/sharetribe

[Invitations] Manage invites

app/helpers/application_helper.rb
@@ -308,7 +308,7 @@ module ApplicationHelper
     }
   end
 
-  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
   # Admin view left hand navigation content
   def admin_links_for(community)
     links = [
@@ -404,6 +404,13 @@ module ApplicationHelper
         :icon_class => icon_class("invite"),
         :path => new_invitation_path,
         :name => "invite_people"
+      },
+      {
+        :topic => :manage,
+        :text => t("admin.communities.invitations.invitations"),
+        :icon_class => icon_class("like"),
+        :path => admin_community_invitations_path(@current_community),
+        :name => "invitations"
       },
       {
         :topic => :configure,
@@ -543,7 +550,7 @@ module ApplicationHelper
 
     links
   end
-  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
   # Settings view left hand navigation content
   def settings_links_for(person, community=nil)

app/services/admin/invitations_service.rb
@@ -1,0 +1,40 @@
+class Admin::InvitationsService
+  attr_reader :community, :params
+
+  def initialize(community:, params:)
+    @params = params
+    @community = community
+  end
+
+  def invitations
+    @invitations ||= resource_scope.order("#{sort_column} #{sort_direction}")
+      .paginate(page: params[:page], per_page: 30)
+  end
+
+  private
+
+  def resource_scope
+    community.invitations.joins(:inviter).includes(:inviter)
+  end
+
+  def sort_column
+    case params[:sort]
+    when 'send_by'
+      'CONCAT(people.given_name, people.family_name)'
+    when 'send_to'
+      'invitations.email'
+    when 'used'
+      'invitations.usages_left'
+    when 'started', nil
+      'invitations.created_at'
+    end
+  end
+
+  def sort_direction
+    if params[:direction] == "asc"
+      "asc"
+    else
+      "desc" #default
+    end
+  end
+end

spec/controllers/admin/community_invitations_controller_spec.rb
@@ -1,0 +1,70 @@
+require 'spec_helper'
+require 'admin/community_invitations_controller'
+
+describe Admin::CommunityInvitationsController, type: :controller do
+  let(:community) { FactoryGirl.create(:community) }
+
+  before(:each) do
+    @request.host = "#{community.ident}.lvh.me"
+    @request.env[:current_marketplace] = community
+    user = create_admin_for(community)
+    sign_in_for_spec(user)
+  end
+
+  describe '#index' do
+    let(:inviter1) { FactoryGirl.create(:person, community_id: community.id, given_name: 'Dawn', family_name: 'Jones') }
+    let(:inviter2) { FactoryGirl.create(:person, community_id: community.id, given_name: 'Tracy', family_name: 'Miler') }
+    let(:invitee2) { FactoryGirl.create(:person, community_id: community.id, given_name: 'Ethel', family_name: 'Harris') }
+    let(:invitation1) do
+      FactoryGirl.create(:invitation, community: community, inviter: inviter1, message: 'Hi, Megan!',
+                                      email: 'megan@example.com', usages_left: 1, created_at: Time.current - 1.minute)
+    end
+    let(:invitation2) do
+      invitation = FactoryGirl.create(:invitation, community: community, inviter: inviter2,
+                                                   message: 'Hi, Ethel!',
+                                                   email: 'ethel@example.com', usages_left: 0)
+      FactoryGirl.create(:community_membership, community: community, person: invitee2,
+                                                invitation_id: invitation.id)
+      invitation
+    end
+
+    it 'shows invitations' do
+      invitation1
+      invitation2
+      get :index, params: {community_id: community.id}
+      invitations = assigns(:service).invitations
+      expect(invitations.size).to eq 2
+      expect(invitations.first).to eq invitation2
+      expect(invitations.last).to eq invitation1
+    end
+
+    it 'shows sort invitations' do
+      invitation1
+      invitation2
+      get :index, params: {community_id: community.id, sort: 'send_by', direction: 'asc'}
+      invitations = assigns(:service).invitations
+      expect(invitations.first).to eq invitation1
+      expect(invitations.last).to eq invitation2
+      get :index, params: {community_id: community.id, sort: 'send_by', direction: 'desc'}
+      invitations = assigns(:service).invitations
+      expect(invitations.first).to eq invitation2
+      expect(invitations.last).to eq invitation1
+      get :index, params: {community_id: community.id, sort: 'send_to', direction: 'asc'}
+      invitations = assigns(:service).invitations
+      expect(invitations.first).to eq invitation2
+      expect(invitations.last).to eq invitation1
+      get :index, params: {community_id: community.id, sort: 'send_to', direction: 'desc'}
+      invitations = assigns(:service).invitations
+      expect(invitations.first).to eq invitation1
+      expect(invitations.last).to eq invitation2
+      get :index, params: {community_id: community.id, sort: 'used', direction: 'asc'}
+      invitations = assigns(:service).invitations
+      expect(invitations.first).to eq invitation2
+      expect(invitations.last).to eq invitation1
+      get :index, params: {community_id: community.id, sort: 'used', direction: 'desc'}
+      invitations = assigns(:service).invitations
+      expect(invitations.first).to eq invitation1
+      expect(invitations.last).to eq invitation2
+    end
+  end
+end

johnchervanev/boxgiver

app/helpers/application_helper.rb
@@ -308,7 +308,7 @@ module ApplicationHelper
     }
   end
 
-  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
   # Admin view left hand navigation content
   def admin_links_for(community)
     links = [
@@ -404,6 +404,13 @@ module ApplicationHelper
         :icon_class => icon_class("invite"),
         :path => new_invitation_path,
         :name => "invite_people"
+      },
+      {
+        :topic => :manage,
+        :text => t("admin.communities.invitations.invitations"),
+        :icon_class => icon_class("invitations"),
+        :path => admin_community_invitations_path(@current_community),
+        :name => "invitations"
       },
       {
         :topic => :configure,
@@ -543,7 +550,7 @@ module ApplicationHelper
 
     links
   end
-  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
   # Settings view left hand navigation content
   def settings_links_for(person, community=nil)

app/services/admin/invitations_service.rb
@@ -1,0 +1,40 @@
+class Admin::InvitationsService
+  attr_reader :community, :params
+
+  def initialize(community:, params:)
+    @params = params
+    @community = community
+  end
+
+  def invitations
+    @invitations ||= resource_scope.order("#{sort_column} #{sort_direction}")
+      .paginate(page: params[:page], per_page: 30)
+  end
+
+  private
+
+  def resource_scope
+    community.invitations.joins(:inviter).includes(:inviter)
+  end
+
+  def sort_column
+    case params[:sort]
+    when 'sent_by'
+      'CONCAT(people.given_name, people.family_name)'
+    when 'sent_to'
+      'invitations.email'
+    when 'used'
+      'invitations.usages_left'
+    when 'started', nil
+      'invitations.created_at'
+    end
+  end
+
+  def sort_direction
+    if params[:direction] == "asc"
+      "asc"
+    else
+      "desc" #default
+    end
+  end
+end

spec/controllers/admin/community_invitations_controller_spec.rb
@@ -1,0 +1,70 @@
+require 'spec_helper'
+require 'admin/community_invitations_controller'
+
+describe Admin::CommunityInvitationsController, type: :controller do
+  let(:community) { FactoryGirl.create(:community) }
+
+  before(:each) do
+    @request.host = "#{community.ident}.lvh.me"
+    @request.env[:current_marketplace] = community
+    user = create_admin_for(community)
+    sign_in_for_spec(user)
+  end
+
+  describe '#index' do
+    let(:inviter1) { FactoryGirl.create(:person, community_id: community.id, given_name: 'Dawn', family_name: 'Jones') }
+    let(:inviter2) { FactoryGirl.create(:person, community_id: community.id, given_name: 'Tracy', family_name: 'Miler') }
+    let(:invitee2) { FactoryGirl.create(:person, community_id: community.id, given_name: 'Ethel', family_name: 'Harris') }
+    let(:invitation1) do
+      FactoryGirl.create(:invitation, community: community, inviter: inviter1, message: 'Hi, Megan!',
+                                      email: 'megan@example.com', usages_left: 1, created_at: Time.current - 1.minute)
+    end
+    let(:invitation2) do
+      invitation = FactoryGirl.create(:invitation, community: community, inviter: inviter2,
+                                                   message: 'Hi, Ethel!',
+                                                   email: 'ethel@example.com', usages_left: 0)
+      FactoryGirl.create(:community_membership, community: community, person: invitee2,
+                                                invitation_id: invitation.id)
+      invitation
+    end
+
+    it 'shows invitations' do
+      invitation1
+      invitation2
+      get :index, params: {community_id: community.id}
+      invitations = assigns(:service).invitations
+      expect(invitations.size).to eq 2
+      expect(invitations.first).to eq invitation2
+      expect(invitations.last).to eq invitation1
+    end
+
+    it 'shows sort invitations' do
+      invitation1
+      invitation2
+      get :index, params: {community_id: community.id, sort: 'sent_by', direction: 'asc'}
+      invitations = assigns(:service).invitations
+      expect(invitations.first).to eq invitation1
+      expect(invitations.last).to eq invitation2
+      get :index, params: {community_id: community.id, sort: 'sent_by', direction: 'desc'}
+      invitations = assigns(:service).invitations
+      expect(invitations.first).to eq invitation2
+      expect(invitations.last).to eq invitation1
+      get :index, params: {community_id: community.id, sort: 'sent_to', direction: 'asc'}
+      invitations = assigns(:service).invitations
+      expect(invitations.first).to eq invitation2
+      expect(invitations.last).to eq invitation1
+      get :index, params: {community_id: community.id, sort: 'sent_to', direction: 'desc'}
+      invitations = assigns(:service).invitations
+      expect(invitations.first).to eq invitation1
+      expect(invitations.last).to eq invitation2
+      get :index, params: {community_id: community.id, sort: 'used', direction: 'asc'}
+      invitations = assigns(:service).invitations
+      expect(invitations.first).to eq invitation2
+      expect(invitations.last).to eq invitation1
+      get :index, params: {community_id: community.id, sort: 'used', direction: 'desc'}
+      invitations = assigns(:service).invitations
+      expect(invitations.first).to eq invitation1
+      expect(invitations.last).to eq invitation2
+    end
+  end
+end