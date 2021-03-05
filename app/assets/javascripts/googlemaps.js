de26174c61709c5dfd870de333e1757ac6f4764b
virat-benzatine <virat@benzatine.com>
2021-03-05 16:27:01 +0530

    Added night mode option to map

--- app/assets/javascripts/googlemaps.js
+++ app/assets/javascripts/googlemaps.js
@@ -495,7 +495,101 @@ function initialize_listing_map(listings, community_location_lat, community_loca
     maxZoom: 17,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
+  const styles = {
+    default: [],
+    night: [
+      { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+      {
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [{ color: "#263c3f" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#6b9a76" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{ color: "#38414e" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#212a37" }],
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#9ca5b3" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ color: "#746855" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#1f2835" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#f3d19c" }],
+      },
+      {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [{ color: "#2f3948" }],
+      },
+      {
+        featureType: "transit.station",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [{ color: "#17263c" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#515c6d" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [{ color: "#17263c" }],
+      },
+    ],
+  };
+  const styleControl = document.getElementById("style-selector-control");
   map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(styleControl);
+  // Set the map's style to the initial value of the selector.
+  const styleSelector = document.getElementById("style-selector");
+  map.setOptions({ styles: styles[styleSelector.value] });
+  // // Apply new JSON when the user selects a different style.
+  styleSelector.addEventListener("change", () => {
+    map.setOptions({ styles: styles[styleSelector.value] });
+  });
+
+
   var prefer_param_loc = (use_community_location_as_default === 'true');
   addListingMarkers(listings, viewport);
 }
