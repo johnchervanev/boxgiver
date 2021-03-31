53d9665aa05054f240fe1c7a663527814112a828
Mahabubul Alam <zikoku07@gmail.com>
2021-03-31 20:38:11 +0600

    feedback from john

--- app/helpers/homepage_helper.rb
+++ app/helpers/homepage_helper.rb
@@ -7,7 +7,7 @@ module HomepageHelper
 
   def with_first_listing_image(listing, &block)
     if listing.listing_images.size == 0
-      block.call(asset_path('nope-not-here.webp'))
+      block.call(asset_path('no_image_available.jpg'))
     else
       Maybe(listing)
         .listing_images
@@ -20,6 +20,15 @@ module HomepageHelper
   def without_listing_image(listing, &block)
     if listing.listing_images.size == 0
       block.call
+    end
+  end
+
+  def love_button(listing, user, klass = '')
+    if user.present?
+      loved = user.is_loving?(listing)
+      link_to "", love_listing_path(listing.id), :class => "icon-heart listing-love right-side #{klass} love-#{listing.id}-listing #{ loved ? 'active' : ''}", :title => "#{loved ? 'Remove from love' : 'Click to love'}", :method => :post, :remote => :true
+    else
+      link_to "", login_path, :class => "icon-heart listing-love #{klass} right-side love-#{listing.id}-listing"
     end
   end
 
