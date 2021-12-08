1dfcc9b0b93870d250bf5755976549792fc8d2e3
Md Nazrul Islam <nazrulku07@gmail.com>
2021-12-08 12:29:25 +0600

    added blog pagination, modified blog banner image

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,7 @@
 class BlogsController < ApplicationController
 
   def index
-    @blogs = Blog.where(published: true)
+    @blogs = Blog.where(published: true).paginate(page: params[:page], per_page: 9)
   end
 
   def show
