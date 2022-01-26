87e1d2867a71e184f0b5453515af9be3ed720fb0
roraman <mailtoamanam@gmail.com>
2022-01-26 13:49:00 +0530

    Add breadcrump in blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,13 @@
 class BlogsController < ApplicationController
+  add_breadcrumb "Home", :landing_page_without_locale_path
 
   def index
+    add_breadcrumb "Blog", blog_index_path
     @blogs = Blog.where(published: true).paginate(page: params[:page], per_page: 9)
   end
 
   def show
     @blog = Blog.friendly.find(params[:id])
+    add_breadcrumb blog.title, blog_path(@blog)
   end
 end
